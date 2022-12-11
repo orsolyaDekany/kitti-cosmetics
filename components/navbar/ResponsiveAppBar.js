8fa64b7aefc543e01b50c7ee1952234c4399701a
Orsolya Dekany <orsolya.dekany@liferay.com>
2022-12-11 22:59:01 +0100

    Add Logo

--- components/navbar/ResponsiveAppBar.js
+++ components/navbar/ResponsiveAppBar.js
@@ -14,11 +14,34 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import { makeStyles } from "@material-ui/core";
+import Image from "next/image";
+import logo from "../../assets/logo.svg";
 
 const drawerWidth = 240;
-const navItems = ["Főoldal", "Munkáim", "Árlista", "Kapcsolat"];
+const navItems = [
+  "Főoldal",
+  "Sminktetoválásról",
+  "Munkáim",
+  "Árlista",
+  "Kapcsolat",
+];
+
+const useStyles = makeStyles({
+  root: {
+    "& .MuiAppBar-root": {
+      boxShadow: "10px 10px 5px 0px #F7A392",
+      backgroundColor: "#FFF6F4",
+      padding: "6px 20px",
+    },
+    "& .MuiButton-root": {
+      padding: "0px 10px",
+    },
+  },
+});
 
 function ResponsiveAppBar(props) {
+  const classes = useStyles();
+
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
 
@@ -47,74 +70,77 @@ function ResponsiveAppBar(props) {
   const container =
     window !== undefined ? () => window().document.body : undefined;
 
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      "& .MuiAppBar-root": {
-        boxShadow: "10px 10px 5px 0px #F7A392",
-        backgroundColor: "#FFF6F4",
-      },
-    },
-  }));
-  const classes = useStyles();
-
   return (
-    <Box sx={{ display: "flex" }}>
-      <div className={classes.root}>
-        <AppBar component="nav">
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              edge="start"
-              onClick={handleDrawerToggle}
-              sx={{ mr: 2, display: { sm: "none" } }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              variant="h6"
-              component="div"
-              sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-            >
-              MUI
-            </Typography>
-            <Box sx={{ display: { xs: "none", sm: "block" } }}>
-              {navItems.map((item) => (
-                <Button key={item} sx={{ color: "#252525" }}>
-                  {item}
-                </Button>
-              ))}
-            </Box>
-          </Toolbar>
-        </AppBar>
-        <Box component="nav">
-          <Drawer
-            container={container}
-            variant="temporary"
-            open={mobileOpen}
-            onClose={handleDrawerToggle}
-            ModalProps={{
-              keepMounted: true, // Better open performance on mobile.
-            }}
-            sx={{
-              display: { xs: "block", sm: "none" },
-              "& .MuiDrawer-paper": {
-                boxSizing: "border-box",
-                width: drawerWidth,
-                backgroundColor: "#FFF6F4",
-              },
-            }}
+    <Box sx={{ display: "flex" }} className={classes.root}>
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="#252525"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
           >
-            {drawer}
-          </Drawer>
-        </Box>
-        <Box component="main" sx={{ p: 3 }}>
-          <Toolbar />
-          <Typography>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            <Image
+              alt="Logo"
+              sx={{
+                flexGrow: 1,
+                display: { xs: "none", sm: "block" },
+              }}
+              src={logo}
+            />
           </Typography>
-        </Box>
-      </div>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {navItems.map((item) => (
+              <Button
+                key={item}
+                sx={{
+                  fontFamily: "Playfair Display",
+                  textTransform: "capitalize",
+                  color: "#252525",
+                  fontSize: "18px",
+                }}
+              >
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+              backgroundColor: "#FFF6F4",
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box component="main" sx={{ p: 3 }}>
+        <Toolbar />
+        <Typography>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </Typography>
+      </Box>
     </Box>
   );
 }
